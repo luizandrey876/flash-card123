e4886bd27d3554cc5c93e78e5980e0dc14dab495
luizandrey876 <luiz.goncalves.freitas@escola.pr.gov.br>
2024-11-29 11:04:28 -0300

    Add files via upload

---
+++ EXPERT-FLASHCARDS-main/perguntas.js
@@ -0,0 +1,144 @@
+criaCartao(
+    'SE LIGA!!!!! P01 <br> EXPERT', 
+    'Qual foi a piada que o fotografo contou?', 
+    'Ninguém sabe, pois ela não foi revelada!'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P02 <br> EXPERT', 
+    'Qual a diferença entre o gato e a Coca-Cola', 
+    'O gato mia, a Coca-cola light.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P03 <br> EXPERT', 
+    'O que o palito de fósforo disse à vela?', 
+    'Sempre perco a cabeça por você.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P04 <br> EXPERT', 
+    'O que são vários pontinhos amarelos na parede?', 
+    'Fandangos alpinistas.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P05 <br> EXPERT', 
+    'O que é, o que é feito para andar e não anda?', 
+    'A Rua.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P06 <br> EXPERT',
+    'O que é que você pode pegar, mas não pode jogar?', 
+    'Resfriado.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P07 <br> EXPERT', 
+    'O que é que você tira a pele, mas quem chora é você.', 
+    'Cebola.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P08 <br> EXPERT', 
+    'Por que tem uma cama elástica no Polo Norte?', 
+    'Para o urso polar. <br> - trocadilho com o verbo pular-.'
+
+)
+
+criaCartao(
+    'SE LIGA!!!!! P09 <br> EXPERT', 
+    'Por que o menino colocou o despertador embaixo da cama?', 
+    'Para acordar em cima da hora.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P10 <br> EXPERT',  
+    'O que é que anda com a barriga para trás?', 
+    'A perna.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P11 <br> EXPERT', 
+    'Por que o louco toma banho com o chuveiro desligado?', 
+    'Porque comprou xampu para cabelos secos.'
+)
+criaCartao(
+    'SE LIGA!!!!! P12 <br> EXPERT',  
+    'O que o zero disse para o oito?', 
+    'Que cinto maneiro!'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P13 <br> EXPERT', 
+    'Em um aquário tem 10 peixes e 5 morreram afogados. Quantos sobraram?', 
+    '10, porque peixe não morre afogado.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P14 <br> EXPERT', 
+    'Como um homem sedentário queima três mil calorias de uma só vez.?', 
+    'Deixando queimar um bolo inteiro.'
+)
+criaCartao(
+    'SE LIGA!!!!! P15 <br> EXPERT', 
+    'Porque o elefante usa óculos vermelho?',
+    'Para ver melhor <br> - ver-me-lho -'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P16 <br> EXPERT', 
+    'Como se chama o rio mais azedo do Brasil?', 
+    'Rio Solimões <br> - Só limões'
+)
+criaCartao(
+    'SE LIGA!!!!! P17 <br> EXPERT', 
+    'O que o tomate foi fazer no banco?', 
+    'Foi tirar um extrato'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P18 <br> EXPERT', 
+    'Por que o pedreiro terminou o namoro?', 
+    'Porque não era concreto'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P19 <br> EXPERT', 
+    'O que é um pontinho vermelho no meio da porta?', 
+    'Um olho mágico com conjuntivite.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P20 <br> EXPERT', 
+    'Porque a foto foi parar na cadeia?', 
+    'Porque ela foi enquadrada.'
+)
+
+criaCartao(
+    'SE LIGA!!!!! P21 <br> EXPERT', 
+    'Qual é o fim da picada?', 
+    'Quando o mosquito vai embora.'
+)
+
+criaCartao (
+    'SE LIGA!!!!! P22 <br> EXPERT', 
+    'Por que a galinha atravessou a rua?', 
+    'Para chegar do outro lado.'
+)
+
+criaCartao (
+    'SE LIGA!!!!! P23 <br> EXPERT', 
+    'Por que as estrelas não fazem miau?', 
+    'Porque Astro-não-mia.<br> - Astronomia -'
+)
+
+criaCartao (
+    'SE LIGA!!!!! P24 <br> EXPERT', 
+    'Quem é a vó da farinha?', 
+    'A veia Quaker.'
+)
+
+
+
